62da9c98854c579568b6dbe2332b01a42e1020d2
Phillip Lord <phillord@haughton.ncl.ac.uk>
2014-06-16 12:04:31 +0100

    Tests reworked to use melpa.

--- dev/linked-buffer-test.el
+++ dev/linked-buffer-test.el
@@ -1,6 +1,15 @@
 (require 'linked-buffer)
 
 
+(defvar linked-buffer-test-dir
+  (concat
+   (file-name-directory
+    (locate-library "linked-buffer.el"))
+   "test/"))
+
+(defun linked-buffer-test-file (filename)
+  (concat linked-buffer-test-dir filename))
+
 (defvar conf-default
   (linked-buffer-default-configuration "bob"))
 
@@ -8,3 +17,10 @@
   (should
    (equal 'fundamental-mode
           (oref conf-default :linked-mode))))
+
+(ert-deftest linked-buffer-simple ()
+  (should
+   (equal "simple\n"
+          (linked-buffer-batch-clone-with-config
+           (linked-buffer-test-file "simple-contents.txt")
+           'linked-buffer-default-init))))
