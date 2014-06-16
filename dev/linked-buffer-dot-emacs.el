9f337f08f219294308750c461e7ecf7c939b9eb3
Phillip Lord <phillord@haughton.ncl.ac.uk>
2014-06-16 12:18:55 +0100

    Initial commit: sort dependencies during test.

---
+++ dev/linked-buffer-dot-emacs.el
@@ -0,0 +1,15 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+(unless (file-exists-p package-user-dir)
+  (package-refresh-contents))
+
+(package-initialize)
+
+(defun ensure-elpa (package)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(ensure-elpa 'dash)
+(ensure-elpa 'm-buffer)
