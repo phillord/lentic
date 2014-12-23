6ac53794c5176d7dac431d00b5c7a8d0d7cdac93
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-12-23 14:25:07 +0100

    More examples added.

---
+++ examples/orgel-org.el
@@ -0,0 +1,17 @@
+;;; orgel-org.el --- A test file -*- lexical-binding: t -*-
+
+;; Author: Phillip Lord
+
+;;; Commentary:
+
+;; First I want to add stuff here because it makes far more sense than adding
+;; it anywhere else. However, I also want to try here, and to me this seems an
+;; awful lot less laggy than it did before. We can add stuff.
+
+;; #+BEGIN_SRC emacs-lisp
+(defun orgel-function ())
+;; #+END_SRC
+
+;; # Local Variables:
+;; # lentic-init: lentic-orgel-org-init
+;; # End:
