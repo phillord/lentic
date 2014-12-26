fb77d5ba4b37ba9a3ccf1e94bd8b2f2354cf828e
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-12-26 19:23:37 +0100

    Examples update to lentic.
    And some other changes to make more readable.

--- examples/block-comment-delayed.clj
+++ examples/block-comment-delayed.clj
@@ -1,29 +1,40 @@
-;; I bet this is going to work really well.  We just type away like a crazy
-;; thing. And while this is happening, nothing else will.
 
+;; This file is similar to block-comment.clj, but uses delayed cloning.
+;; That is all the lentic functionality runs on the idle cycle. Originally,
+;; this was a good idea, as lentic.el was rather inefficient, and could make
+;; emacs lag. Now this is probably less efficient and isn't really necessary.
 
-;; And the other way around also seems to be slowed. The pabbrev thing is really,
-;; really odd?
-
-;; Now on the other hand this is a little bit more feasible. In fact, this
-;; seems perfectly reasonable to me
-
-;; Interestingly, pabbrev stuff doesn't seem to work at all well.
-;; Well, things seen to be going okay now. Wonder why the pabbbrev thing is
-;; not working? And what happens the other way around. Well?, it looks pretty
-;; good. In fact, too good, because it's not running on the idle cyle, I am sure. 
 
 ;; \begin{code}
 (println "hello world")
 ;; \end{code}
 
-;; So let us see what happens here. So, some parts of what we need to do are
-;; not great, and that eff
+;; Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit
+;; tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et,
+;; mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis
+;; parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.
+;; Nullam tristique diam non turpis. Cras placerat accumsan nulla. Nullam rutrum.
+;; Nam vestibulum accumsan nisl.
 
-;; So one thing that would really nice to sort out is this.
+;; Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit
+;; tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et,
+;; mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis
+;; parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.
+;; Nullam tristique diam non turpis. Cras placerat accumsan nulla. Nullam rutrum.
+;; Nam vestibulum accumsan nisl.
+
+;; Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit
+;; tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et,
+;; mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis
+;; parturient montes, nascetur ridiculus mus. Nulla posuere. Donec vitae dolor.
+;; Nullam tristique diam non turpis. Cras placerat accumsan nulla. Nullam rutrum.
+;; Nam vestibulum accumsan nisl.
+
+
+;;
 
 ;; %%
 ;; %% Local Variables:
-;; %% linked-buffer-init: linked-buffer-clojure-latex-delayed-init
+;; %% lentic-init: lentic-clojure-latex-delayed-init
 ;; %% End:
 ;; %%
--- examples/block-comment.clj
+++ examples/block-comment.clj
@@ -1,45 +1,39 @@
-;; Let us put some more stuff in. And clone the other way.
+;; This buffer shows a "block-comment" where we wish to comment large blocks
+;; of the buffer between one lentic buffer and the other.
 
-;; So, how are we doing?
-
-;; Now let us try down here to see whether it is all going good.
+;; In this case, we use latex tags to indicate the code blocks. Outside of
+;; these tags, we start every line with comments. Inside we do not (although
+;; we leave comments that are there.
 
 ;; \begin{code}
 (println "It is now broken")
 ;; \end{code}
 
-;; And now it is back again!
+;; We can have multiple code blocks, of course, after the first.
 
-;; Let us type some stuff in here. 
-
-;; Looks pretty good to me!
-;; sfds more
 ;; \begin{code}
 (println "hello")
-
 ;; \end{code}
 
-;; Let us put some stuff on the end
-
-;; Now we can put some more stuff in.
+;; And more
 
 ;; \begin{code}
 (println "Hello")
 ;; \end{code}
 
-;; Hello
-;;
-;; Bummer
-;;
+;; And more
+
 ;; \begin{code}
-(println "hello") 
+(println "hello")
 ;; \end{code}
 
-
-
+;; And we can put more comments after the final text. And finally, we finish
+;; with a file local variable to tell lentic what kind of text this is. Note
+;; that start characters which are comments in both clojure and latex. In real
+;; use dir-local variables work better.
 
 ;; %%
 ;; %% Local Variables:
-;; %% linked-buffer-init: linked-buffer-clojure-latex-init
+;; %% lentic-init: lentic-clojure-latex-init
 ;; %% End:
 ;; %%
