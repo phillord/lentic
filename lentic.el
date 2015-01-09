ec9bcd0d64cbbf245bbc16878f4282af1d5d4b1c
Phillip Lord <phillip.lord@newcastle.ac.uk>
2015-01-09 15:28:28 +0000

    Update for 0.6.1 release.

--- lentic.el
+++ lentic.el
@@ -6,7 +6,7 @@
 
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
-;; Version: 0.6
+;; Version: 0.6.1
 ;; Package-Requires: ((emacs "24")(m-buffer "0.8")(dash "2.5.0"))
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
@@ -85,7 +85,9 @@
 ;; (global-lentic-start-mode)
 
 ;; to your .emacs. Or alternatively, you can install manually and add
+
 ;; (require 'lentic-autoloads)
+
 ;; first.
 
 
