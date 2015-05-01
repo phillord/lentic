3f20d2a5ec33c046db3ba384e717de7ff57fef19
Phillip Lord <phillip.lord@newcastle.ac.uk>
2015-05-01 19:34:42 +0100

    Prepare for next iteration
    Which is 0.9 and not 0.13 as implied by the last commit message!

--- lentic.el
+++ lentic.el
@@ -6,7 +6,7 @@
 
 ;; Author: Phillip Lord <phillip.lord@newcastle.ac.uk>
 ;; Maintainer: Phillip Lord <phillip.lord@newcastle.ac.uk>
-;; Version: 0.9
+;; Version: 0.10
 ;; Package-Requires: ((emacs "24.4")(m-buffer "0.10")(dash "2.5.0")(f "0.17.2"))
 
 ;; The contents of this file are subject to the GPL License, Version 3.0.
