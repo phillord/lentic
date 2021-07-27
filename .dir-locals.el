f1f77812b0d0aae21e0cee8f7cea14432a21241e
Phillip Lord <phillip.lord@russet.org.uk>
2021-07-27 13:42:48 +0100

    Add markdown example

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((emacs-lisp-mode
-  .
-  ((lentic-init . lentic-orgel-org-init)
-   (eval . (require 'm-buffer-macro nil t)))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((lentic-init . lentic-orgel-org-init)
+                     (eval . (require 'm-buffer-macro nil t)))))
---
+++ examples/clojure-markdown.clj
@@ -0,0 +1,11 @@
+;; This is markdown and this is working very nicely
+
+;; ```{.clojure}
+(println "hello big world")
+;; ```
+
+
+
+;; Local Variables:
+;; lentic-init: lentic-clojure-markdown-init
+;; End:
